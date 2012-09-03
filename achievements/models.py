3e645644820f398c23053b91a20411b1f42c53f2
Kathrin Wende <kathrin.wende@redmoonstudios.de>
2012-09-03 12:20:29 +0200

    add class Trophies add import validate

--- achievements/models.py
+++ achievements/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from filebrowser.fields import FileBrowseField
 from django.utils.translation import ugettext_lazy as _
-
+import validate
 
 class Category(models.Model):
     name = models.CharField(_("Name"), max_length=255)
@@ -42,3 +42,11 @@ class TaskProgress(models.Model):
 
 class CollectionAchievement(Achievement):
     achievements = models.ManyToManyField(Achievement, related_name="collection_achievements")
+
+class Trophies(models.Model):
+    achievement = models.ForeignKey(Achievement, blank=True)
+    user = models.ForeignKey(User)
+    position = models.PositiveIntegerField(validators=[validate.validate_max()])
+
+    class Meta:
+        unique_together = ("user","position")
