e60eb1908d8ed77865e70247f101fe01f93d8670
Kathrin Wende <kathrin.wende@redmoonstudios.de>
2012-09-04 14:44:29 +0200

    change validation error string

--- achievements/validate.py
+++ achievements/validate.py
@@ -3,4 +3,4 @@ import settings
 
 def validate_max(value):
         if value > settings.SET_PARAMETER:
-            raise ValidationError(u'There are only %s positions' % settings.SET_PARAMETER)
+            raise ValidationError(u'There are only 0-%s positions' % settings.SET_PARAMETER)
