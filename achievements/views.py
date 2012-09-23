e322d93d23fe97449635ff0d5b2c41a49298c703
Caddy <caddy@caddy-pc.(none)>
2012-09-23 21:38:38 +0200

    add for-loops for progressbar data

--- achievements/views.py
+++ achievements/views.py
@@ -18,9 +18,18 @@ def Overview(request):
 def CategoryView(request, category_id):
     category_list = Category.objects.filter(parent_category__isnull=True)
     all_categories = Category.objects.all()
+    children = Category.objects.filter(parent_category__id = category_id)
     achievement_list = Achievement.objects.filter(category__id = category_id)
     achievements_accomplished = Achievement.objects.filter(users = request.user)
+    for cat in all_categories:
+        if cat.id == int(category_id):
+            progress = Category.progressbar(cat, request.user)
+            all_bar = Category.all_progressbar(cat, request.user)
+    child_progress = []
+    for child in children:
+        child_progress.append(Category.progressbar(child, request.user))
     return render_to_response('achievements/category.html',
                               {'category_list': category_list,'all_categories': all_categories, 
+                               'progress': progress, 'child_progress': child_progress, 'all_bar': all_bar,
                                'achievement_list': achievement_list, 'achievements_accomplished': achievements_accomplished, 'categoryID': category_id}, 
                               context_instance=RequestContext(request))
