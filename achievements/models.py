6837da7bec93ce144efc3fce38b4aa43e7033560
Caddy <caddy@caddy-pc.(none)>
2012-09-23 21:32:13 +0200

    comment all subclasses of Achievement

--- achievements/models.py
+++ achievements/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-#from filebrowser.fields import FileBrowseField
+from filebrowser.fields import FileBrowseField
 from django.utils.translation import ugettext_lazy as _
 import validate
 
@@ -20,7 +20,7 @@ class Category(models.Model):
     
     def __unicode__(self):
         if (self.parent_category != None):
-            return "%s -> %s" % (self.parent_category, self.name)
+            return "%s - %s" % (self.parent_category, self.name)
         else:
             return "%s" % (self.name)
 
@@ -28,45 +28,24 @@ class Achievement(models.Model):
     name = models.CharField(_("Name"), max_length=255)
     description = models.TextField(_("Description"))
     points = models.IntegerField(blank=False, default=0)
-    #icon = FileBrowseField(_("Icon"), directory='icons/', format='image', max_length=255, blank=True)
+    icon = FileBrowseField(_("Icon"), directory='icons/', format='image', max_length=255, blank=True)
     category = models.ForeignKey(Category, related_name="achievements")
+    users = models.ManyToManyField(User, related_name="user_achievements", blank=True)
     
     def __unicode__(self):
         return self.name
-
-class UserAchievement(Achievement):
-    users = models.ManyToManyField(User, related_name="achievements")
-
-class ProgressAchievement(Achievement):
-    required_amount = models.PositiveIntegerField()
-    users = models.ManyToManyField(User, related_name="progress_achievements", through="Progress")
-
-class Progress(models.Model):
-    user = models.ForeignKey(User)
-    progress_achievement = models.ForeignKey(ProgressAchievement)
-    achieved_amount = models.PositiveIntegerField()
-
-class Task(models.Model):
-    name = models.CharField(_("Name"), max_length=255)
-    description = models.TextField(_("Description"))
     
-    def __unicode__(self):
-        return self.name
-
-class TaskAchievement(Achievement):
-    tasks = models.ManyToManyField(Task)
-    users = models.ManyToManyField(User, related_name="task_achievements", through="TaskProgress")
-
-class TaskProgress(models.Model):
-    user = models.ForeignKey(User)
-    task_achievement = models.ForeignKey(TaskAchievement)
-    completed_tasks = models.ManyToManyField(Task, limit_choices_to={})
+    #def get_subclass(self):
+        #if hasattr(self, 'progressachievement'):
+            #return self.progressachievement
+        #if hasattr(self, 'taskachievement'):
+            #return self.taskachievement
+        #if hasattr(self, 'collectionachievement'):
+            #return self.collectionachievement
     
-    def __unicode__(self):
-        return self.task_achievement.name
-
-class CollectionAchievement(Achievement):
-    achievements = models.ManyToManyField(Achievement, related_name="collection_achievements")
+    def render(self):
+        output = "<p><b>%s</b></p><p>%s</p>" % (self.name, self.description)
+        return output
 
 class Trophies(models.Model):
     achievement = models.ForeignKey(Achievement, blank=True)
