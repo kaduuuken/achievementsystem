1a1f8e420075203320a5786672bb6de291e13f55
Caddy <caddy@caddy-pc.(none)>
2012-09-23 21:27:10 +0200

    add settings for MEDIA Data

--- settings.py
+++ settings.py
@@ -1,7 +1,6 @@
 from os import path
 
 PROJECT_PATH = path.abspath(path.join(path.dirname(__file__), path.pardir))
-print PROJECT_PATH
 # Django settings for achievementsystem project.
 
 DEBUG = True
@@ -49,18 +48,18 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = path.join(PROJECT_PATH, 'achievementsystem/achievements/media/')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ""
+STATIC_ROOT = path.join(PROJECT_PATH, 'static/')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -76,7 +75,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    path.join(PROJECT_PATH, 'achievements/static/'),
+    'achievements/static/',
 )
 
 # List of finder classes that know how to find static files in
@@ -105,13 +104,13 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'achievementTest.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    path.join(PROJECT_PATH, 'achievementTest/templates/'),
+    path.join(PROJECT_PATH, 'achievements/templates/'),
 )
 
 INSTALLED_APPS = (
@@ -127,8 +126,8 @@ INSTALLED_APPS = (
     # 'django.contrib.admindocs',
     'achievements',
     #requirements
-    #'filebrowser',
-    'mptt',
+    'filebrowser',
+    'grappelli',
 )
 
 # A sample logging configuration. The only tangible logging
