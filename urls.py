b47f00dec8e4d00bac1459bc01b2eb6d771642ba
Caddy <caddy@caddy-pc.(none)>
2012-09-23 21:26:11 +0200

    add new urls(grappelli,MEDIA_ROOT)

--- urls.py
+++ urls.py
@@ -2,10 +2,13 @@ from django.conf.urls.defaults import patterns, include, url
 from filebrowser.sites import site
 from django.contrib import admin
 admin.autodiscover()
+import settings
 
 urlpatterns = patterns('',
     url(r'^achievements/', include('achievements.urls', namespace='achievements')),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/filebrowser/', include(site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
     (r'^admin/jsi18n', 'django.views.i18n.javascript_catalog'),
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
 )
