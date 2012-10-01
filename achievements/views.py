405794187bb3f45459e6140bf188fa1966c6085f
Caddy <caddy@caddy-pc.(none)>
2012-10-01 21:22:23 +0200

    rewrite Overview

--- achievements/views.py
+++ achievements/views.py
@@ -9,28 +9,34 @@ import settings
 @login_required(login_url='/admin')
 def Overview(request):
     category_list = Category.objects.filter(parent_category__isnull=True)
-    all_categories = Category.objects.all()
+    all_achievements_count = float(0)
+    all_achievements = Achievement.objects.all()
+    for ach in all_achievements:
+        all_achievements_count += 1
+    achievements_count = 0
     achievements_accomplished = Achievement.objects.filter(users = request.user)
+    for ach in achievements_accomplished:
+        achievements_count += 1
+    if all_achievements_count != 0:
+        all_percent = int((achievements_count/all_achievements_count)*100)
+        all_achievements_count = int(all_achievements_count)
+    else:
+        all_percent = 0
     trophies_list = [None]*(settings.SET_PARAMETER+1)
     trophies = Trophies.objects.all()
     for trophy in trophies:
         trophies_list[trophy.position] = trophy.achievement
-    for cat in all_categories:
-        if cat.parent_category is None:
-            #progress = Category.progressbar(cat, request.user)
-            for child in cat.child_categories.all():
-                all_child_bar = Category.all_progressbar(child, request.user)
-                print all_child_bar
+    for cat in category_list:
             all_bar = Category.all_progressbar(cat, request.user)
             all_count = float(Category.count_all_achievements(cat))
             if all_count != 0:
-                all_percent = int((all_bar/all_count)*100)
+                all_cat_percent = int((all_bar/all_count)*100)
             else:
-                all_percent = 0
+                all_cat_percent = 0
     return render_to_response('achievements/index.html',
                               {'category_list': category_list, 'trophies_list': trophies_list, 'all_bar': all_bar, 
-                               'all_percent': all_percent, 'all_child_bar': all_child_bar,
-                               'achievements_accomplished': achievements_accomplished},
+                               'all_percent': all_percent, 'all_cat_percent': all_cat_percent,#'all_child_bar': all_child_bar,
+                               'achievements_count': achievements_count, 'all_achievements_count': all_achievements_count},
                               context_instance=RequestContext(request))
 
 def TrophyView(request):
@@ -49,6 +55,7 @@ def TrophyCategoryView(request, category_id):
                               context_instance=RequestContext(request))
 
 def CategoryView(request, category_id):
+    current_category = Category.objects.get(id = category_id)
     category_list = Category.objects.filter(parent_category__isnull=True)
     all_categories = Category.objects.all()
     children = Category.objects.filter(parent_category__id = category_id)
@@ -71,8 +78,9 @@ def CategoryView(request, category_id):
     child_progress = []
     for child in children:
         child_progress.append(Category.progressbar(child, request.user))
+    trophies_list = [None]*(settings.SET_PARAMETER+1)
     return render_to_response('achievements/category.html',
-                              {'category_list': category_list,'all_categories': all_categories, 
+                              {'category_list': category_list,'all_categories': all_categories, 'trophies_list': trophies_list,
                                'progress': progress, 'child_progress': child_progress, 'all_bar': all_bar, 'all_percent': all_percent, 'pro_percent': pro_percent,
-                               'achievement_list': achievement_list, 'achievements_accomplished': achievements_accomplished, 'categoryID': category_id}, 
+                               'achievement_list': achievement_list, 'achievements_accomplished': achievements_accomplished,'current_category': current_category, 'categoryID': category_id}, 
                               context_instance=RequestContext(request))
