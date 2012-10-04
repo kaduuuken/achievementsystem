4907b4bd6322c3779c4135d2ae6c776dd4e99cdf
Caddy <caddy@caddy-pc.(none)>
2012-10-04 20:03:26 +0200

    change function name

--- achievements/validate.py
+++ achievements/validate.py
@@ -2,5 +2,5 @@ from django.core.exceptions import ValidationError
 import settings
 
 def validate_max(value):
-        if value > settings.SET_PARAMETER:
-            raise ValidationError(u'There are only 0-%s positions' % settings.SET_PARAMETER)
+        if value >= settings.TROPHY_COUNT:
+            raise ValidationError(u'There are only 0-%s positions' % (settings.TROPHY_COUNT-1))
