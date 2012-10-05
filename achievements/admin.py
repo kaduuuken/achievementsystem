52c68065af138e5d355d30ad1d39d5c73219fbe0
Kathrin Wende <kathrin.wende@redmoonstudios.de>
2012-10-05 15:31:21 +0200

    add ProgressAdmin

--- achievements/admin.py
+++ achievements/admin.py
@@ -1,4 +1,4 @@
-from models import Achievement, Category, Trophy#, CollectionAchievement, Progress, ProgressAchievement, Task, TaskAchievement, TaskProgress, UserAchievement
+from models import Achievement, Category, Trophy, CollectionAchievement, Progress, ProgressAchievement, Task, TaskAchievement, TaskProgress
 from django.contrib import admin
 from django.contrib.admin.widgets import FilteredSelectMultiple
 from django.db import models
@@ -11,43 +11,51 @@ class AchievementAdmin(admin.ModelAdmin):
     list_display=['name', 'description', 'category']
     search_fields = ('name', 'category')
 
-#class ProgressAchievementAdmin(admin.ModelAdmin):
-    #list_display=['name', 'description', 'category']
-    #search_fields = ('name', 'category')
+class ProgressAdmin(admin.ModelAdmin):
+    list_display=['progress_achievement', 'achieved_amount', 'user']
 
-#class TaskAchievementAdmin(admin.ModelAdmin):
-    #list_display=['name', 'description', 'category']
-    #search_fields = ('name', 'category')
-    #formfield_overrides = {
-        #models.ManyToManyField: {'widget': FilteredSelectMultiple("tasks", False)}
-    #}
+class ProgressAchievementAdmin(admin.ModelAdmin):
+    list_display=['name', 'description', 'category']
+    search_fields = ('name', 'category')
+    formfield_overrides = {
+        models.ManyToManyField: {'widget': FilteredSelectMultiple("user", False)}
+    }
 
-#class CollectionAchievementAdmin(admin.ModelAdmin):
-    #list_display=['name', 'description', 'category']
-    #search_fields = ('name', 'category')
-    #formfield_overrides = {
-        #models.ManyToManyField: {'widget': FilteredSelectMultiple("achievements", False)}
-    #}
+class TaskAchievementAdmin(admin.ModelAdmin):
+    list_display=['name', 'description', 'category']
+    search_fields = ('name', 'category')
+    formfield_overrides = {
+        models.ManyToManyField: {'widget': FilteredSelectMultiple("tasks", False)}
+    }
+    formfield_overrides = {
+        models.ManyToManyField: {'widget': FilteredSelectMultiple("users", False)}
+    }
 
-#class TaskAdmin(admin.ModelAdmin):
-    #list_display=['name', 'description']
+class CollectionAchievementAdmin(admin.ModelAdmin):
+    list_display=['name', 'description', 'category']
+    search_fields = ('name', 'category')
+    formfield_overrides = {
+        models.ManyToManyField: {'widget': FilteredSelectMultiple("achievements", False)}
+    }
 
-#class TaskProgressAdmin(admin.ModelAdmin):
-    #list_display=['task_achievement', 'user']
-    #formfield_overrides = {
-        #models.ManyToManyField: {'widget': FilteredSelectMultiple("tasks", False)}
-    #}
+class TaskAdmin(admin.ModelAdmin):
+    list_display=['name', 'description']
+
+class TaskProgressAdmin(admin.ModelAdmin):
+    list_display=['task_achievement', 'user']
+    formfield_overrides = {
+        models.ManyToManyField: {'widget': FilteredSelectMultiple("tasks", False)}
+    }
 
 class TrophiesAdmin(admin.ModelAdmin):
     list_display=['achievement', 'position']
 
 admin.site.register(Achievement, AchievementAdmin)
 admin.site.register(Category, CategoryAdmin)
-#admin.site.register(ProgressAchievement, ProgressAchievementAdmin)
-#admin.site.register(Progress)
-#admin.site.register(TaskAchievement, TaskAchievementAdmin)
-#admin.site.register(Task, TaskAdmin)
-#admin.site.register(TaskProgress, TaskProgressAdmin)
+admin.site.register(ProgressAchievement, ProgressAchievementAdmin)
+admin.site.register(Progress, ProgressAdmin)
+admin.site.register(TaskAchievement, TaskAchievementAdmin)
+admin.site.register(Task, TaskAdmin)
+admin.site.register(TaskProgress, TaskProgressAdmin)
 admin.site.register(Trophy, TrophiesAdmin)
-#admin.site.register(UserAchievement)
-#admin.site.register(CollectionAchievement, CollectionAchievementAdmin)
+admin.site.register(CollectionAchievement, CollectionAchievementAdmin)
