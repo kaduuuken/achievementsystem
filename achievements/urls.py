5fb2fd770f22b37148daf0d1a0d8cd778d054e69
Kathrin Wende <kathrin.wende@redmoonstudios.de>
2012-11-13 15:28:17 +0100

    add comments

--- achievements/urls.py
+++ achievements/urls.py
@@ -3,11 +3,17 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from models import Category
 from views import Overview
 
+# urls for views in view.py
 urlpatterns = patterns('achievements.views',
+    # url for Overview
     url(r'^$', 'Overview', name='overview'),
+    # url for CategoryView
     url(r'^(?P<category_id>\d+)/$', 'CategoryView', name='category'),
+    # url for PositionModalView
     url(r'^position_remote/(?P<achievement_id>\d+)/$', 'PositionModalView', name='position_modal'),
+    # url for TrophyModalView
     url(r'^trophy_remote/(?P<trophy_pos>\d+)/$', 'TrophyModalView', name='trophy_modal'),
+    # url for TrophyView
     url(r'^(?P<trophy_slot>\d+)/(?P<achievement_id>\d+)/$', 'TrophyView', name='trophy'),
 )
 
