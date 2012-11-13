c7fe1a86028d6765647a7319f0dc96f591373268
Kathrin Wende <kathrin.wende@redmoonstudios.de>
2012-11-13 15:24:22 +0100

    add comments, comment admin/jsi18n

--- urls.py
+++ urls.py
@@ -4,11 +4,14 @@ from django.contrib import admin
 admin.autodiscover()
 import settings
 
-
 urlpatterns = patterns('',
+    # including urls which are needed in the views.py (achievements.urls)
     url(r'^achievements/', include('achievements.urls', namespace='achievements')),
+    # url for activating filebrowser in admin site
     url(r'^admin/filebrowser/', include(site.urls)),
+    # url for activating Django admin site
     url(r'^admin/', include(admin.site.urls)),
-    (r'^admin/jsi18n', 'django.views.i18n.javascript_catalog'),
+    #(r'^admin/jsi18n', 'django.views.i18n.javascript_catalog'),
+    # url for media path in filebrowser
     (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
 )
