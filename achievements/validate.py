88c8a10a543e569690237132d0d2eee3954cbe7b
Kathrin Wende <kathrin.wende@redmoonstudios.de>
2012-11-13 15:28:01 +0100

    add comments

--- achievements/validate.py
+++ achievements/validate.py
@@ -1,6 +1,7 @@
 from django.core.exceptions import ValidationError
 import settings
 
+# checks, if given value is higher than the set amounts of positions (set in achievements/settings.py)
 def validate_max(value):
         if value >= settings.TROPHY_COUNT:
             raise ValidationError(u'There are only 0-%s positions' % (settings.TROPHY_COUNT-1))
